49232a3ab62046deca6061ecc2d60f2f65e57ee9
Ram Prasad <mramprasad007@gmail.com>
2025-06-28 21:01:39 +0530

    fix tslint issues

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+} else {
+  console.error("Root element not found");
+}
